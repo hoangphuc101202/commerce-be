bae5ea6d4c10e9dc53119e1cf13e93f5510298f9
Hoang Phuc NGuyen <hoangphuc1996vnvn@gmail.com>
2024-11-10 13:32:23 +0700

    fix: program.cs

--- Restapi-net8/Program.cs
+++ Restapi-net8/Program.cs
@@ -10,7 +10,6 @@ using Serilog;
 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 // Add services to the container.
 builder.Services.AddCors(option => {
