f95c1e160a89821873bc024a86d3d674fc63fecd
Hoang Phuc NGuyen <hoangphuc1996vnvn@gmail.com>
2024-12-15 00:14:52 +0700

    fix: validate email user search

--- Restapi-net8/Model/DTO/Users/GetAllUsers.cs
+++ Restapi-net8/Model/DTO/Users/GetAllUsers.cs
@@ -2,6 +2,5 @@ using System.ComponentModel.DataAnnotations;
 
 public class GetAllUsers{
     public string? fullName { get; set; }
-    [EmailAddress(ErrorMessage = "Invalid Email Address")]
     public string? email { get; set; }
 }
